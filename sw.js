479683dfc8b54a0ba7f6928bce20e73aa8fd52d2
yeouidoeat <yeouido.eat@gmail.com>
2026-05-23 20:48:35 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'cafeteria-map-v1';
-const urlsToCache = ['/', '/index.html', '/logo.png', '/favicon.png'];
+const CACHE_NAME = 'cafeteria-map-v3';
+const urlsToCache = ['/logo.png', '/favicon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
